a53f85fec7e0c835e80e58d014f88483b8ff603c
inspired-octo <another_sf@126.com>
2021-07-07 22:39:48 +0800

    grafana

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,7 @@
 #none
+# 20210705 https://github.com/inspired-octo/Python.git
+- fork python from github 
+- VSCODE beautify https://glitchbone.github.io/vscode-base16-term/#/atelier-forest
+- first project: data report tools integrating with 
+- grafana https://play.grafana.org/d/000000014/elasticsearch-metrics?orgId=1
+- manual: 
--- python_demo.py
+++ python_demo.py
@@ -1,3 +1,13 @@
 import os
 import time
+import requests
+import pandas as pd
+from pprint import pprint
+from lxml import etree
+import time
+import warnings
+warnings.filterwarnings("ignore")
 
+print("[Log ERROR]:\033[1;31;23mthis is a hello world!\033[0m")
+print("[Log WARNING]:\033[1;33;23mthis is a hello world!\033[0m")
+print("[Log INFO]:\033[1;32;23mthis is a hello world!\033[0m")
