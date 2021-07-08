1fa88c9d6827e97d3f8fd77a191e7c7e6d00c1ba
inspired-octo <another_sf@126.com>
2021-07-08 22:41:43 +0800

    integrate commonroad_api

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,6 +2,10 @@
 # 20210705 https://github.com/inspired-octo/Python.git
 - fork python from github 
 - VSCODE beautify https://glitchbone.github.io/vscode-base16-term/#/atelier-forest
+
 - first project: data report tools integrating with 
 - grafana https://play.grafana.org/d/000000014/elasticsearch-metrics?orgId=1
 - manual: 
+
+- switch version of python between 2 and 3
+- https://blog.csdn.net/hero_myself/article/details/88745888
--- python_demo.py
+++ python_demo.py
@@ -1,3 +1,5 @@
+from commonroad_io.commonroad import common
+from commonroad_io.commonroad.common.file_reader import CommonRoadFileReader
 import os
 import time
 import requests
@@ -8,6 +10,19 @@ import time
 import warnings
 warnings.filterwarnings("ignore")
 
-print("[Log ERROR]:\033[1;31;23mthis is a hello world!\033[0m")
-print("[Log WARNING]:\033[1;33;23mthis is a hello world!\033[0m")
-print("[Log INFO]:\033[1;32;23mthis is a hello world!\033[0m")
+import os
+import matplotlib.pyplot as plt
+
+file_path = os.path.join(os.getcwd(), 'commonroad_io/commonroad/scenarios/NGSIM/Lankershim/USA_Lanker-1_1_T-1.xml')
+
+scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
+
+# plt.figure(figsize=(25, 10))
+# draw_object(scenario)
+# draw_object(planning_problem_set)
+# plt.gca().set_aspect('equal')
+# plt.show()
+
+print("[Log ERROR]:\033[1;31;23m red!\033[0m")
+print("[Log WARNING]:\033[1;33;23m yellow!\033[0m")
+print("[Log INFO]:\033[1;32;23mtgreen!\033[0m")
