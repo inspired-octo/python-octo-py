f208f825f78ed7d8fa3dd6864d48a8cbdf35c671
inspired-octo <another_sf@126.com>
2021-07-12 22:51:03 +0800

    0721 add robotframework

--- python_demo.py
+++ python_demo.py
@@ -9,19 +9,39 @@ from lxml import etree
 import time
 import warnings
 warnings.filterwarnings("ignore")
+from IPython import display
 
 import os
 import matplotlib.pyplot as plt
+from commonroad_io.commonroad.visualization.draw_dispatch_cr import draw_object
 
-file_path = os.path.join(os.getcwd(), 'commonroad_io/commonroad/scenarios/NGSIM/Lankershim/USA_Lanker-1_1_T-1.xml')
+# file_path = os.path.join(os.getcwd(), 'commonroad-scenarios/scenarios/NGSIM/Lankershim/USA_Lanker-1_1_T-1.xml')
 
-scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
+# scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
 
 # plt.figure(figsize=(25, 10))
 # draw_object(scenario)
-# draw_object(planning_problem_set)
+# # draw_object(planning_problem_set)
 # plt.gca().set_aspect('equal')
 # plt.show()
+file_path = '/Users/shifei/Desktop/octo-rupin/python-octo-py/commonroad-scenarios/scenarios/interactive/hand-crafted/ZAM_Tutorial-1_1_I-1-1/ZAM_Tutorial-1_1_I-1-1.cr.xml'
+
+
+# read in the scenario and planning problem set
+scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
+
+# plot the scenario for 40 time step, here each time step corresponds to 0.1 second
+for i in range(0, 40):
+    # uncomment to clear previous graph
+    # display.clear_output(wait=True)
+    
+    plt.figure(figsize=(20, 10))
+    # plot the scenario at different time step
+    draw_object(scenario, draw_params={'time_begin': i})
+    # plot the planning problem set
+    draw_object(planning_problem_set)
+    plt.gca().set_aspect('equal')
+    plt.show()
 
 print("[Log ERROR]:\033[1;31;23m red!\033[0m")
 print("[Log WARNING]:\033[1;33;23m yellow!\033[0m")
